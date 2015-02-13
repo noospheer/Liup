18f2cc3cf1d19ad3193b00c48590372ce10e7590
Lachlan J. Gunn <lachlan.gunn@adelaide.edu.au>
2015-02-13 10:52:34 +1030

    Added further docstrings.

--- src/liuproto/endpoint.py
+++ src/liuproto/endpoint.py
@@ -4,10 +4,9 @@ import numpy
 import numpy.random
 import numpy.fft
 
+
 class Physics(object):
-    """
-    Implementation of the "physical" part of the Liu key agreement protocol.
-    """
+    """Implementation of an endpoint of the Liu key agreement protocol."""
     def __init__(self, number_of_exchanges, reflection_coefficient, cutoff, ramp_time, seed=None):
         self.number_of_exchanges = number_of_exchanges
         self.reflection_coefficient = reflection_coefficient
@@ -15,12 +14,25 @@ class Physics(object):
         self.ramp_time = ramp_time
 
         self.random_state = numpy.random.RandomState(seed=seed)
-        self.U1 = self.__generate_random_values()
-        self.U2 = self.__generate_random_values()
+        self.random_values = []
+        self.correlation_sum = 0.0
+        self.current_exchange = 0
+
+        self.reset()
+
+    def reset(self):
+        """Reset the endpoint to its initial random state."""
+        if self.random_state.rand() < 0.5:
+            self.reflection_coefficient = -self.reflection_coefficient
+
+        self.random_values = self.__generate_ramped_random_values()
+
+        self.correlation_sum = 0.0
 
         self.current_exchange = 0
 
     def __generate_random_values(self):
+        """Generate band-limited white noise, returning a real numpy array."""
         white_noise = self.random_state.randn(self.number_of_exchanges+1)
         white_noise_frequency_domain = numpy.fft.fft(white_noise)
 
@@ -29,18 +41,47 @@ class Physics(object):
 
         return numpy.real(numpy.fft.ifft(white_noise_frequency_domain))
 
+    def __generate_ramped_random_values(self):
+        """Generate ramped random processes, returning a numpy array."""
+        u1 = self.__generate_random_values()
+        u2 = self.__generate_random_values()
+
+        ramp = self.__ramp_function(numpy.arange(len(u1)))
+
+        return u1*numpy.sqrt(1-ramp**2) + u2*ramp
+
     def __ramp_function(self, n):
-        if n >= self.ramp_time:
-            return 1.0
-        else:
-            return 0.5*(1+numpy.sin((float(n)-self.ramp_time/2.0)*numpy.pi/float(self.ramp_time)))
+        """Compute the raised-sine ramp function, returning a numpy array."""
+        ramp = numpy.ones(len(n))
+        transition_values = numpy.array(n) < self.ramp_time
+        if len(transition_values) == 1:
+            transition_values = 0
+        ramp[transition_values] = \
+            0.5*(1+numpy.sin(
+                        (numpy.array(n[transition_values]).astype(float)
+                            - self.ramp_time/2.0
+                            )*numpy.pi/float(self.ramp_time)
+                    )
+                )
+
+        return ramp
 
     def exchange(self, incoming):
-        ramp = self.__ramp_function(self.current_exchange)
-        injected_signal = self.U1[self.current_exchange]*(1-ramp) + self.U2[self.current_exchange]*ramp
+        """Perform a single exchange, returning a floating-point response."""
+        ramp = self.__ramp_function([self.current_exchange])[0]
 
         ramped_reflection_coefficient = self.reflection_coefficient*ramp
 
+        if self.current_exchange > 0:
+            self.correlation_sum += \
+                self.random_values[self.current_exchange - 1] * incoming
+
+        new_message = self.random_values[self.current_exchange] \
+                      + incoming*ramped_reflection_coefficient
         self.current_exchange += 1
 
-        return injected_signal + incoming*ramped_reflection_coefficient
+        return new_message
+
+    def estimate_other(self):
+        """Estimate the state of the other endpoint, returning a boolean."""
+        return self.correlation_sum > 0
--- src/liuproto/internallink.py
+++ src/liuproto/internallink.py
@@ -1,1 +1,36 @@
-__author__ = 'lgunn'
+#!/usr/bin/env python
+
+
+class InternalLink(object):
+    """A link controller for two endpoints in the same process."""
+    def __init__(self, physics_A, physics_B):
+
+        self.physics_A = physics_A
+        self.physics_B = physics_B
+        self.messages = []
+
+    def run_proto(self):
+        """Run a single iteration of the protocol."""
+
+        self.physics_A.reset()
+        self.physics_B.reset()
+
+        self.messages = []
+
+        self.messages.append(self.physics_A.exchange(0.0))
+
+        for i in range(self.physics_A.number_of_exchanges):
+            self.messages.append(self.physics_B.exchange(self.messages[-1]))
+            self.messages.append(self.physics_A.exchange(self.messages[-1]))
+
+        if not (self.physics_A.estimate_other()
+                    ^ (self.physics_A.reflection_coefficient > 0)):
+            return None
+
+        elif not (self.physics_B.estimate_other()
+                    ^ (self.physics_B.reflection_coefficient > 0)):
+            return None
+
+        else:
+            return (not self.physics_A.estimate_other(),
+                    self.physics_B.estimate_other())
