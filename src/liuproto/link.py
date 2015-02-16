1e5c6480dbe57f6602baa057f910bf402c5fdcf7
Lachlan J. Gunn <lachlan.gunn@adelaide.edu.au>
2015-02-16 16:22:09 +1030

    Added initial networking support.

--- src/liuproto/link.py
+++ src/liuproto/link.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 import endpoint
+import SocketServer
+import socket
+import json
+import sys
+
 
 class InternalLink(object):
     """A link controller for two endpoints in the same process."""
@@ -34,3 +39,109 @@ class InternalLink(object):
         else:
             return (not self.physics_A.estimate_other(),
                     self.physics_B.estimate_other())
+
+
+class NetworkLinkRequestHandler(SocketServer.BaseRequestHandler):
+    """A server implementing the Liu protocol over the network"""
+
+    def handle(self):
+        self.server.physics = []
+
+        while True:
+
+            config = self.__read_json_string()
+
+            if config == '{}':
+                return
+
+            # Now that we have a valid configuration string, produce our
+            # endpoint.
+            physics = endpoint.Physics.from_json(config)
+
+            # Finally, run the protocol.
+            for i in range(physics.number_of_exchanges):
+                message = json.loads(self.__read_json_string())
+                result = physics.exchange(message['message'])
+                sys.stdout.flush()
+                message_out = json.dumps({'message': result})
+                self.request.send(message_out)
+
+            message = json.loads(self.__read_json_string())
+            physics.exchange(message['message'])
+            self.request.send('{}')
+
+            if physics.estimate_other() != (physics.reflection_coefficient > 0):
+                self.server.physics.append(physics.estimate_other())
+            else:
+                self.server.physics.append(None)
+
+    def __read_json_string(self):
+        json_string = ''
+
+        # Keep reading until we have a valid JSON string.
+        while True:
+            try:
+                json_string += self.request.recv(1024)
+                json.loads(json_string)
+                break
+            except ValueError:
+                pass
+
+        return json_string
+
+
+class NetworkServerLink(object):
+    def __init__(self, address):
+        self.server = SocketServer.TCPServer(address, NetworkLinkRequestHandler)
+        self.server.physics = []
+
+    def run_proto(self):
+        self.server.handle_request()
+        retval = []
+
+        return self.server.physics
+
+
+class NetworkClientLink(object):
+    def __init__(self, address, physics):
+        self.address = address
+        self.physics = physics
+        self.client_socket = socket.socket()
+        self.client_socket.connect(self.address)
+
+    def run_proto(self):
+        self.physics.reset()
+
+        self.client_socket.send(self.physics.to_json())
+        self.client_socket.send(json.dumps({'message': self.physics.exchange(0.0)}))
+        for i in range(self.physics.number_of_exchanges):
+            message = json.loads(self.__read_json_string(self.client_socket))
+            self.client_socket.send(json.dumps({
+                'message': self.physics.exchange(message['message'])}))
+
+        self.__read_json_string(self.client_socket)
+
+        if self.physics.estimate_other() \
+                == (self.physics.reflection_coefficient > 0):
+
+            return None
+        else:
+            return self.physics.reflection_coefficient > 0
+
+    def close(self):
+        self.client_socket.send('{}')
+        self.client_socket.close()
+
+    def __read_json_string(self, client_socket):
+        json_string = ''
+
+        # Keep reading until we have a valid JSON string.
+        while True:
+            json_string += client_socket.recv(1024)
+            try:
+                json.loads(json_string)
+                break
+            except ValueError:
+                pass
+
+        return json_string
